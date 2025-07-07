8f97ecdda28ede4dcaf90a8b6e2acf568886205c
aswinsuriya16 <aswinsuriya16@gmail.com>
2025-07-07 12:39:59 +0000

    data account creation for frontend

---
+++ client/types.ts
@@ -0,0 +1,14 @@
+import * as borsh from 'borsh';
+
+class CounterAccount {
+    count = 0;
+    constructor({count} : {count:number}){
+        this.count = count;
+    }
+}
+
+const schema = {struct : {
+    count : 'u32'
+}};
+
+export const COUNTER_SIZE = borsh.serialize(schema,new CounterAccount({count : 0})).length;
