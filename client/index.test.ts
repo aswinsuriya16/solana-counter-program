55d8a839f4a7cd61ac4037dd1810af88bc670c06
aswinsuriya16 <aswinsuriya16@gmail.com>
2025-07-09 22:38:10 +0530

    Increment and decrement

--- client/index.test.ts
+++ client/index.test.ts
@@ -1,10 +1,11 @@
 import {expect , test} from "bun:test";
-import {Connection,Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction} from '@solana/web3.js'
-import { COUNTER_SIZE, schema } from "./types";
+import {Connection,Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction, TransactionInstruction} from '@solana/web3.js'
+import { COUNTER_SIZE, schema , CounterAccount} from "./types";
 import * as borsh from 'borsh';
 
 let adminAccount = Keypair.generate();
 let dataAccount = Keypair.generate();
+const programid = new PublicKey("BfpAUSM6eEgFK9BLxj7iZfNB5RUyCbA47CvmcutmEPmv"); // Deployed program Id
 
 test("Account is initialized",async()=>{
     //Airdrop
@@ -14,7 +15,6 @@ test("Account is initialized",async()=>{
     const data = await connection.getAccountInfo(adminAccount.publicKey);
     //create account 
     const lamports = await connection.getMinimumBalanceForRentExemption(COUNTER_SIZE); 
-    const programid = new PublicKey("BfpAUSM6eEgFK9BLxj7iZfNB5RUyCbA47CvmcutmEPmv"); // Deployed program Id
     const txn = new Transaction().add(
         SystemProgram.createAccount({
             fromPubkey : adminAccount.publicKey,
@@ -26,12 +26,65 @@ test("Account is initialized",async()=>{
     );
     const signature = await connection.sendTransaction(txn,[adminAccount,dataAccount]);
     await connection.confirmTransaction(signature);
-    //console.log(adminAccount.publicKey.toBase58());
-    // console.log(dataAccount.publicKey.toBase58());
+    
+    console.log(adminAccount.publicKey.toBase58());
+    console.log(dataAccount.publicKey.toBase58());
 
     const counterAccount = await connection.getAccountInfo(dataAccount.publicKey);
     if(!counterAccount) { 
         throw new Error("Account does not exist");
     }
-    const counter = borsh.deserialize(schema,counterAccount.data)
+    const counter = borsh.deserialize(schema,counterAccount.data) as CounterAccount;
+    console.log(counter);
 })
+
+test("Increment",async()=>{
+    // we need to add the data to the blockchain
+    const connection = new Connection("http://127.0.0.1:8899");
+    const incrementvalue = 5;
+    const txn = new Transaction().add({
+        keys : [
+            {
+                pubkey : dataAccount.publicKey,
+                isSigner : false,
+                isWritable : true
+            }
+        ],
+        programId : programid,
+        data : Buffer.from(new Uint8Array([0,5,0,0,0]))
+    });
+    const sig = await connection.sendTransaction(txn,[adminAccount]);
+    await connection.confirmTransaction(sig);
+    console.log(sig);
+    const counterAccount = await connection.getAccountInfo(dataAccount.publicKey);
+    if(!counterAccount) {
+        throw new Error("");
+    }
+    const counter = borsh.deserialize(schema,counterAccount.data) as CounterAccount;
+    console.log(counter);
+    console.log(counter.count);
+});
+
+test("Decrement",async()=>{
+    const connection = new Connection("http://127.0.0.1:8899");
+    const txn = new Transaction().add({
+        keys : [
+            {
+                pubkey : dataAccount.publicKey,
+                isSigner : false,
+                isWritable : true
+            }
+        ],
+        programId : programid,
+        data : Buffer.from(new Uint8Array([1,3,0,0,0]))
+    });
+    const sig = await connection.sendTransaction(txn,[adminAccount]);
+    await connection.confirmTransaction(sig);
+    const counterAccount = await connection.getAccountInfo(dataAccount.publicKey);
+    if(!counterAccount) {
+        throw new Error("...")
+    }
+    const counter = borsh.deserialize(schema,counterAccount.data) as CounterAccount;
+    console.log(counter);
+    console.log(counter.count);
+});
